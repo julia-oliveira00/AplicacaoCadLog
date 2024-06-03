a74e9171ca6c269bb944bdad789171670e5d8096
julia-oliveira00 <almeidajuliaoliveira00@gmail.com>
2024-06-03 09:25:01 -0300

    cria html base e formulario de cadastro

---
+++ cad.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+
+<body>
+    <div class="singup">
+        <form class="form">
+            <div>
+                <label for='name'>Nome</label><br>
+                <input type='text' id='name' name='name' placeholder="Nome completo" autocomplete="on">
+            </div>
+            <div>
+                <label for='email'>E-mail</label><br>
+                <input type='text' id='email' placeholder="" autocomplete="on"><br>
+            </div>
+            <div>
+                <label for='password'>Senha</label><br>
+                <input type='password' id='password' autocomplete="on"><br>
+            </div>
+            <div>
+                <label for='password'>Confirme a Senha</label><br>
+                <input type='password' id='passwordconfirm' autocomplete="on"><br>
+            </div><br>
+            <div id="button">
+                <button onclick="">SALVAR</button>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
