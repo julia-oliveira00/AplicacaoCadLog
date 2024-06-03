6487000ab0675b07593aef405d1bc20ff64639da
julia-oliveira00 <almeidajuliaoliveira00@gmail.com>
2024-06-03 09:25:22 -0300

    cria html base e formulario de login

---
+++ log.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <div class="singup">
+        <form class="form">
+            <div>
+                <label for='email'>E-mail</label><br>
+                <input type='text' id='email' placeholder="" autocomplete="on"><br>
+            </div>
+            <div>
+                <label for='password'>Senha</label><br>
+                <input type='password' id='password' autocomplete="on"><br>
+            </div>
+            <div id="button">
+                <button onclick="">ENTRAR</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
