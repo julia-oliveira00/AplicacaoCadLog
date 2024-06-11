4941ce4614072afaf90cc227a192591088ec87aa
julia-oliveira00 <almeidajuliaoliveira00@gmail.com>
2024-06-11 07:49:54 -0300

    processo de alteração das escritas do site

--- index.php
+++ index.php
@@ -191,9 +191,8 @@
         </header>
 
         <main class="px-3">
-            <h1>Cover your page.</h1>
-            <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit
-                the text, and add your own fullscreen background photo to make it your own.</p>
+            <h1>Seja bem-vind@.</h1>
+            <p class="lead">Essa página é a página principal de um pequeno projeto criado para o ensino de acesso a base de dados utilizando PHP como linguagem de back-end. Acesse o repositório do projeto no</p>
             <p class="lead">
                 <a href="#" class="btn btn-lg btn-light fw-bold border-white bg-white">Learn more</a>
             </p>
